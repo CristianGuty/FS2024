49023cfb3cf37cf9d4cf15c94dccff49294b3f43
CristianGuty <gutycuervo@hotmail.com>
2024-08-25 16:00:38 -0300

    commit al 25/8

---
+++ Modulo5/archivAsincronos.js
@@ -0,0 +1,15 @@
+var fs = require('fs'); // Llamar modulo fs.
+// Llamar de manera sincrona.
+
+var alTerminar = (err, contenido) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(contenido);
+}
+
+fs.readFile(__dirname + '/robot.js', 'utf8', alTerminar);
+
+
+console.log('Fin!');

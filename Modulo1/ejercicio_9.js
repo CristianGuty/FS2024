3903ecb7778ff1c8026e5db8cac3d8256d076b68
CristianGuty <gutycuervo@hotmail.com>
2024-08-30 16:06:49 -0300

    Ejercicio 9 terminado

--- Modulo1/ejercicio_9.js
+++ Modulo1/ejercicio_9.js
@@ -5,6 +5,7 @@ usuario y ser naturales < 11. Debe calcular el promedio e imprimir en pantalla s
 let notas = [];
 let respuesta = Boolean;
 let bandera = 0;
+
 respuesta = (confirm("Escriba las 5 notas del alumno por favor:"));
 if (respuesta == true) {
     for (i=0; i<5; i++) {
@@ -18,30 +19,34 @@ if (respuesta == true) {
                 alert("Ese numero no puede ser ingresado, por favor digite un numero del 1 - 10");
                 notas[i] = prompt(`Escriba la nota numero ${i+1} del alumno.`);
                 }
-        }
-       
+            } 
     }
-        if (bandera = 0){ //si ingrese correctamente los 5 numeros calculo el promedio.
-            let arrayDeNumeros = notas.map(Number); //transformo el array a valores numericos.
-                function calcularPromedio(notas) { //hago una funcion que acumule los numeros del array automaticamente sin hacer un for y luego q calcule el promedio de todos los numeros ingresados.
-                    let suma = arrayDeNumeros.reduce((acumulador, valorActual) => acumulador + valorActual);
-                    return suma / arrayDeNumeros.length;
-                }
-                alert(`${notas}`); // a modo de control si las notas que ingrese estan bien.
-                let promedio = calcularPromedio(arrayDeNumeros);
-                alert(`El promedio de notas del alumno es ${promedio}`);
-            /* switch (promedio){
-                    case "0 & < 6":
-                        let desempeño = alert(`El promedio del alumno fue ${promedio} y lamentablemente esta Reprobado.`);
-                    case ">= 6 & <=8":
-                        desempeño = alert(`El promedio del alumno fue ${promedio} y esta felizmente aprobado!`);
-                    case "> 8 & <= 10":
-                        desempeño = alert(`El promedio del alumno fue ${promedio} y tuvo un desempeño sobresaliente!!` )
-                }*/
+
+    if (bandera == 0){ //si ingrese correctamente los 5 numeros calculo el promedio.
+        let arrayDeNumeros = notas.map(Number); //transformo el array a valores numericos.
+            
+        function calcularPromedio(notas) { //hago una funcion que acumule los numeros del array automaticamente sin hacer un for y luego q calcule el promedio de todos los numeros ingresados.
+        let suma = arrayDeNumeros.reduce((acumulador, valorActual) => acumulador + valorActual);
+        return suma / arrayDeNumeros.length;
         }
+            alert(`Usted ingreso las siguientes notas: ${arrayDeNumeros}`); // a modo de control si las notas que ingrese estan bien.
+            let promedio = calcularPromedio(arrayDeNumeros);
+            //alert(`El promedio de notas del alumno es ${promedio}`);
+            switch (true) {
+                case (promedio < 5):
+                    alert(`El promedio del alumno fue ${promedio} y lamentablemente está Reprobado.`);
+                    break;
+                case (promedio >= 6 && promedio <= 8):
+                    alert(`El promedio del alumno fue ${promedio} y está felizmente aprobado!`);
+                    break;
+                case (promedio > 8 && promedio <= 10):
+                    alert(`El promedio del alumno fue ${promedio} y tuvo un desempeño sobresaliente!!`);
+                    break;
+                default: //no entendi xq pense q era un error de tipeo tuyo cuando pedias < 5 y luego > a 6, x eso yo directamente puse reprobado < 6. Lo corrijo como vos pediste.
+                    alert(`El promedio del alumno fue ${promedio}, por favor hablar con el profesor para una posibilidad de salir de condicional.`)
+            }
+
+    }
 } else {
     alert("buenas tardes");
 }
-
-
-    

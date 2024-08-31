38ac9690c1cfd6704edade49206aecc0debe4274
CristianGuty <gutycuervo@hotmail.com>
2024-08-31 16:08:18 -0300

    Correcciones al ejercicio 10 y 11

---
+++ Modulo1/ejercicio_12.js
@@ -0,0 +1,12 @@
+/*Crear un archivo con el nombre “ejercicio_12.js” que contenga el código JavaScript donde cree una
+clase base y luego hereden de ella para crear una clase hija. La clase representará algo general, como un
+vehículo, y la clase hija podría ser un tipo específico de vehículo, como un automóvil. En este ejercicio
+deberán realizar los siguientes pasos
+ Definir una clase base Vehiculo con tres atributos (marca, modelo y año) y una función
+(obtenerInformacion) para mostrar información del vehículo.
+ Crear una clase hija Automovil que hereda de Vehiculo y añadir dos nuevos atributos (color y precio).
+También se sobrescribe el método obtenerInformacion para incluir la información específica del
+automóvil.
+ Crear instancias de ambas clases y mostrar la información en la consola del navegador.*/
+
+
